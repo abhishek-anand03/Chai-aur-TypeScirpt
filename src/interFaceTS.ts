61b40ab611ed7ddf652d27643210e8195fd60f36
Abhishek Anand <ab.work2103@gmail.com>
2026-07-20 14:52:18 +0530

    learn about InterFaces v/s Type in TypeScript

--- src/interFaceTS.ts
+++ src/interFaceTS.ts
@@ -14,3 +14,41 @@ function serveChai(order: ChaiOrder) {
   console.log(order);
 }
 
+// Interfaces v/s Types:-
+type TeaRecipe = {
+  water: number;
+  milk: number;
+}
+
+// interface TeaRecipe{
+//   water: number;
+//   milk: number;
+// }
+
+class MasalaChai implements TeaRecipe {
+  water = 100;
+  milk = 50;
+}
+
+/* type CupSize = "small" | "large";
+
+class Chai implements CupSize {
+  // Class cannot implement literal types, it can only implement object or intersection type.
+}
+*/
+
+interface CupSize {
+  size: "small" | "large";
+}
+
+class Chai implements CupSize {
+  size: "small" | "large" = "large";
+}
+
+  // Example with boolean type:-
+/*type Response = {ok: true} | {ok: false};
+
+class myRes implements Response {
+  ok: boolean = true;
+}
+*/
