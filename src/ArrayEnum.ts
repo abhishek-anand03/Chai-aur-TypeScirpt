86cfb2a57b89575a500b7542357fd78d40e6f5f5
Abhishek Anand <ab.work2103@gmail.com>
2026-07-21 21:32:00 +0530

    learn to use Arrays in TypeScript

--- src/ArrayEnum.ts
+++ src/ArrayEnum.ts
@@ -6,3 +6,14 @@ const ChaiPrice: number[] = [10, 20];
 
 // Literal Value form with Utility Type example:-
 const rating: Array<number> = [4.5, 5.0];
+
+// Usage:-
+type Chai = {
+  name: string;
+  price: number;
+}
+
+const menu: Chai[] = [
+  {name: "Masala", price: 15},
+  {name: "Adrak", price: 25}
+]
