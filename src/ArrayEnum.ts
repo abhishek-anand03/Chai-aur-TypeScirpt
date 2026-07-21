b9a3f9ed5b81f137f24b7eb56c2dbba47cf1ede4
Abhishek Anand <ab.work2103@gmail.com>
2026-07-21 21:29:06 +0530

    learn Literal Value form with Utility Type of Array in TypeScript

--- src/ArrayEnum.ts
+++ src/ArrayEnum.ts
@@ -4,3 +4,5 @@
 const ChaiFlavours: string[] = ["Masala", "Adrak"];
 const ChaiPrice: number[] = [10, 20];
 
+// Literal Value form with Utility Type example:-
+const rating: Array<number> = [4.5, 5.0];
