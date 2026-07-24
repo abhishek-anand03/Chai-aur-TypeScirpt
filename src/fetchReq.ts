b192de5467a1bdb41d609a50418862ee57e6170a
Abhishek Anand <ab.work2103@gmail.com>
2026-07-24 13:10:14 +0530

    Example of fetching data using 'fetch' Method

---
+++ src/fetchReq.ts
@@ -0,0 +1,20 @@
+// Example of fetching data using "fetch" Method:-
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+const fetchData = async () => {
+  try {
+    const response = await fetch("");
+    if(!response.ok) {
+      throw new Error(`HTTP error ${response.status}`);
+    }
+    const data: Todo = await response.json();
+  }
+  catch (error: any) {
+
+  }
+}
